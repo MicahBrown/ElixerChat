1afb5a9144463942540bf8723d62622155fd19de
Micah Brown <micahyb@gmail.com>
2017-01-29 11:27:48 -0600

    added transitions between control displays

--- web/static/js/views/loader.js
+++ web/static/js/views/loader.js
@@ -1,9 +1,11 @@
 import MainView    from './main';
 import ChatShowView from './chat/show';
+import PageIndexView from './page/index';
 
 // Collection of specific view modules
 const views = {
   ChatShowView,
+  PageIndexView,
 };
 
 export default function loadView(viewName) {
---
+++ web/static/js/views/page/index.js
@@ -0,0 +1,41 @@
+import MainView from '../main';
+import {findAncestor} from '../../utils';
+
+export default class View extends MainView {
+  mount() {
+    super.mount();
+    loadControl();
+  }
+
+  unmount() {
+    super.unmount();
+  }
+}
+
+let initializeButton = function(button){
+  let top    = findAncestor(button, "control-display")
+  let bottom = top.nextElementSibling
+  let column = findAncestor(top, "control-column")
+
+  button.onclick = function(){
+    let displayHeight = top.offsetHeight
+    column.style.minHeight = displayHeight + "px"
+    Velocity(top, {opacity: 0}, 500, function(){
+      top.style.display = "none"
+      bottom.style.opacity = 0
+      bottom.style.display = "block"
+      Velocity(bottom, {opacity: 1}, 500, function(){
+        column.style.minHeight = null
+      })
+    })
+    return false;
+  }
+}
+
+var loadControl = function(){
+  let buttons = document.getElementsByClassName("control-button")
+  for (var button of buttons) {
+    initializeButton(button)
+  }
+  console.log("loaded")
+}
