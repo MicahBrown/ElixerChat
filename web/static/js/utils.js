989e148d45869199798bf4ed438656a37b1ca78d
Micah Brown <micahyb@gmail.com>
2017-02-04 00:07:24 -0600

    added util function for escaping html

--- web/static/js/utils.js
+++ web/static/js/utils.js
@@ -36,3 +36,21 @@ export function findAncestor(el, className) {
 export function insertAfter(referenceNode, newNode) {
   referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
+
+
+let entityMap = {
+  '&': '&amp;',
+  '<': '&lt;',
+  '>': '&gt;',
+  '"': '&quot;',
+  "'": '&#39;',
+  '/': '&#x2F;',
+  '`': '&#x60;',
+  '=': '&#x3D;'
+};
+
+export function escapeHtml(string) {
+  return String(string).replace(/[&<>"'`=\/]/g, function(s) {
+    return entityMap[s];
+  });
+}
