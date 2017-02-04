4b1a6726cf6e0dfc90149f2a79a6a486c2a6efb2
Micah Brown <micahyb@gmail.com>
2017-02-04 04:14:09 -0600

    extracted template code into the view

--- web/views/message_view.ex
+++ web/views/message_view.ex
@@ -25,4 +25,21 @@ defmodule Daychat.MessageView do
   def display_body(message) do
     message.body |> Phoenix.HTML.Format.text_to_html
   end
+
+  def user_token(message), do: token(message.user)
+  defp token(nil), do: "BOT"
+  defp token(user), do: user.token
+
+  def participant_color(message, participants) do
+    participant = Enum.find(participants, fn(p) -> p.user_id == message.user_id end)
+    color(participant)
+  end
+  defp color(nil), do: "#000000"
+  defp color(participant), do: Daychat.Participant.color(participant)
+
+  def author(message, participants) do
+    content = Phoenix.HTML.Tag.content_tag(:i, "", class: "fa fa-user-circle")
+    content = raw("<i class='fa fa-user-circle'></i> #{user_token(message)}")
+    Phoenix.HTML.Tag.content_tag(:span, content, style: "color: #{participant_color(message, participants)}")
+  end
 end
