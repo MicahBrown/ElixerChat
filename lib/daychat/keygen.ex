c0ec975a91e7daca990f48c0074217be20af9878
Micah Brown <micahyb@gmail.com>
2017-02-04 00:41:27 -0600

    renamed module for generating auth tokens

--- lib/daychat/keygen.ex
+++ lib/daychat/keygen.ex
@@ -1,14 +1,14 @@
-defmodule Tokenizer do
+defmodule Keygen do
   @size 11
   @charset "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ" |> String.codepoints # Base58
 
   def generate do
-    "" |> make_token(@size)
+    "" |> make_key(@size)
   end
 
-  defp make_token(token, n) when n <= 0, do: token
-  defp make_token(token, n) do
+  defp make_key(token, n) when n <= 0, do: token
+  defp make_key(token, n) do
     Enum.random(@charset) <> token
-    |> make_token(n - 1)
+    |> make_key(n - 1)
   end
 end
