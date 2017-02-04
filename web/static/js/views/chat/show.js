d2fd5a21d69049516489f32a17ca56ae57eb6703
Micah Brown <micahyb@gmail.com>
2017-02-04 07:05:04 -0600

    fix clock references

--- web/static/js/views/chat/show.js
+++ web/static/js/views/chat/show.js
@@ -319,6 +319,8 @@ let loadHeaderLinks = function(){
 }
 
 let loadClock = () => {
+  let clock = document.getElementById('clock')
+
   function getTimeRemaining(endtime) {
     let t = Date.parse(endtime) - Date.parse(new Date());
     let seconds = Math.floor((t / 1000) % 60);
@@ -334,8 +336,9 @@ let loadClock = () => {
     };
   }
 
-  function initializeClock(id, endtime) {
-    let clock = document.getElementById(id);
+  let clockInterval;
+
+  function initializeClock(clock, endtime) {
     let hoursSpan = clock.querySelector('.hours');
     let minutesSpan = clock.querySelector('.minutes');
     let secondsSpan = clock.querySelector('.seconds');
@@ -343,21 +346,21 @@ let loadClock = () => {
     function updateClock() {
       let t = getTimeRemaining(endtime);
 
-      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      hoursSpan.innerHTML   = ('0' + t.hours).slice(-2);
       minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
       secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
       if (t.total <= 0) {
-        clearInterval(timeinterval);
+        clearInterval(clockInterval);
       }
     }
 
     updateClock();
-    let timeinterval = setInterval(updateClock, 1000);
+    clockInterval = setInterval(updateClock, 1000);
   }
 
   let deadline = clock.dataset.deadline
   let m = moment.utc(deadline)
 
-  initializeClock('clock', m.toDate());
+  initializeClock(clock, m.toDate())
 }
