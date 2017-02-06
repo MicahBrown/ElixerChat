6e9c0c7fdb33651d3602fe3fd6ff97bfad4427b9
Micah Brown <micahyb@gmail.com>
2017-02-05 18:03:39 -0600

    don't calculate unnecessities

--- web/static/js/views/chat/show.js
+++ web/static/js/views/chat/show.js
@@ -308,10 +308,10 @@ let loadClock = () => {
     let seconds = Math.floor((t / 1000) % 60);
     let minutes = Math.floor((t / 1000 / 60) % 60);
     let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    let days = Math.floor(t / (1000 * 60 * 60 * 24));
+    // let days = Math.floor(t / (1000 * 60 * 60 * 24));
     return {
-      'total': t,
-      'days': days,
+      // 'total': t,
+      // 'days': days,
       'hours': hours,
       'minutes': minutes,
       'seconds': seconds
