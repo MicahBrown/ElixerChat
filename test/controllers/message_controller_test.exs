d2a910cdab2202ba7ba556a13ad237c84ee57281
Micah Brown <micahyb@gmail.com>
2017-02-08 00:42:16 -0600

    removed specs related to unused actions

--- test/controllers/message_controller_test.exs
+++ test/controllers/message_controller_test.exs
@@ -2,6 +2,7 @@ defmodule Daychat.MessageControllerTest do
   use Daychat.ConnCase
 
   alias Daychat.Message
+  alias Daychat.Chat
   @valid_attrs %{body: "some content"}
   @invalid_attrs %{}
 
@@ -9,54 +10,16 @@ defmodule Daychat.MessageControllerTest do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
 
-  test "lists all entries on index", %{conn: conn} do
-    conn = get conn, message_path(conn, :index)
-    assert json_response(conn, 200)["data"] == []
-  end
-
-  test "shows chosen resource", %{conn: conn} do
-    message = Repo.insert! %Message{}
-    conn = get conn, message_path(conn, :show, message)
-    assert json_response(conn, 200)["data"] == %{"id" => message.id,
-      "user_id" => message.user_id,
-      "chat_id" => message.chat_id,
-      "body" => message.body}
-  end
-
-  test "renders page not found when id is nonexistent", %{conn: conn} do
-    assert_error_sent 404, fn ->
-      get conn, message_path(conn, :show, -1)
-    end
-  end
-
   test "creates and renders resource when data is valid", %{conn: conn} do
-    conn = post conn, message_path(conn, :create), message: @valid_attrs
+    chat = Repo.insert! %Chat{}
+    conn = post conn, chat_message_path(conn, :create, chat), message: @valid_attrs
     assert json_response(conn, 201)["data"]["id"]
     assert Repo.get_by(Message, @valid_attrs)
   end
 
   test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-    conn = post conn, message_path(conn, :create), message: @invalid_attrs
+    chat = Repo.insert! %Chat{}
+    conn = post conn, chat_message_path(conn, :create, chat), message: @invalid_attrs
     assert json_response(conn, 422)["errors"] != %{}
-  end
-
-  test "updates and renders chosen resource when data is valid", %{conn: conn} do
-    message = Repo.insert! %Message{}
-    conn = put conn, message_path(conn, :update, message), message: @valid_attrs
-    assert json_response(conn, 200)["data"]["id"]
-    assert Repo.get_by(Message, @valid_attrs)
-  end
-
-  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
-    message = Repo.insert! %Message{}
-    conn = put conn, message_path(conn, :update, message), message: @invalid_attrs
-    assert json_response(conn, 422)["errors"] != %{}
-  end
-
-  test "deletes chosen resource", %{conn: conn} do
-    message = Repo.insert! %Message{}
-    conn = delete conn, message_path(conn, :delete, message)
-    assert response(conn, 204)
-    refute Repo.get(Message, message.id)
   end
 end
