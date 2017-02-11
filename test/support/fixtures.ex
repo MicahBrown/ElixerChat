2c385cd00d05865c84616379e83b1fcce4c5ef41
Micah Brown <micahyb@gmail.com>
2017-02-11 00:12:11 -0600

    added fixtures for users and participants

--- test/support/fixtures.ex
+++ test/support/fixtures.ex
@@ -1,15 +1,26 @@
 defmodule Daychat.Fixtures do
   alias Daychat.Repo
+  alias Daychat.User
   alias Daychat.Chat
+  alias Daychat.Participant
 
-  def fixture(:chat) do
-    %Chat{
-      token: AuthKeyGenerator.generate,
-      auth_key: AuthKeyGenerator.generate
-    }
+  def fixture(name, attrs \\ %{})
+  def fixture(:user, attrs) do
+    User.changeset(%User{}, attrs)
   end
 
-  def fixture!(name) do
-    Repo.insert! fixture(name)
+  def fixture(:chat, attrs) do
+    user = fixture!(:user)
+    Chat.changeset(%Chat{user: user}, attrs)
+  end
+
+  def fixture(:participant, attrs) do
+    user = fixture!(:user)
+    chat = fixture!(:chat)
+    Participant.changeset(%Participant{user: user, chat: chat}, attrs)
+  end
+
+  def fixture!(name, attrs \\ %{}) do
+    Repo.insert! fixture(name, attrs)
   end
 end
