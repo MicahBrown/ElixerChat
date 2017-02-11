b3c85bb529d898b7c2197fcc020f62553401a43f
Micah Brown <micahyb@gmail.com>
2017-02-10 21:19:26 -0600

    removed comments

--- test/controllers/search_controller_test.exs
+++ test/controllers/search_controller_test.exs
@@ -16,15 +16,3 @@ defmodule Daychat.SearchControllerTest do
     assert json_response(conn, 200) == %{"data" => nil}
   end
 end
-
-# defmodule Daychat.SearchController do
-#   use Daychat.Web, :controller
-
-#   alias Daychat.Chat
-
-#   def show(conn, %{"q" => token}) do
-#     chat = Repo.get_by(Chat, token: token)
-
-#     render(conn, "show.json", chat: chat)
-#   end
-# end
