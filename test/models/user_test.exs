6f90b7ef862988111bff10cf575f61426b1189bc
Micah Brown <micahyb@gmail.com>
2017-02-11 17:51:39 -0600

    no invalid user changeset

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -4,15 +4,9 @@ defmodule Daychat.UserTest do
   alias Daychat.User
 
   @valid_attrs %{name: "some content", token: "some content"}
-  @invalid_attrs %{}
 
   test "changeset with valid attributes" do
     changeset = User.changeset(%User{}, @valid_attrs)
     assert changeset.valid?
-  end
-
-  test "changeset with invalid attributes" do
-    changeset = User.changeset(%User{}, @invalid_attrs)
-    refute changeset.valid?
   end
 end
