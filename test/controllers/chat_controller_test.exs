e22c03ed10932a2d80340f287340f50945ebcd60
Micah Brown <micahyb@gmail.com>
2017-02-11 14:59:46 -0600

    added chat controller specs

--- test/controllers/chat_controller_test.exs
+++ test/controllers/chat_controller_test.exs
@@ -1,66 +1,59 @@
 defmodule Daychat.ChatControllerTest do
   use Daychat.ConnCase
 
+  import Daychat.Fixtures
+
+  alias Daychat.Repo
   alias Daychat.Chat
-  @valid_attrs %{participants_count: 42, token: "some content"}
+  alias Daychat.Participant
+  @valid_attrs %{}
   @invalid_attrs %{}
 
-  test "lists all entries on index", %{conn: conn} do
-    conn = get conn, chat_path(conn, :index)
-    assert html_response(conn, 200) =~ "Listing chats"
+  test "shows expired status on index", %{conn: conn} do
+    conn = get conn, expired_chat_path(conn, :index)
+    assert html_response(conn, 200) =~ "ChatExpiredView"
   end
 
-  test "renders form for new resources", %{conn: conn} do
+  test "redirects to root on new", %{conn: conn} do
     conn = get conn, chat_path(conn, :new)
-    assert html_response(conn, 200) =~ "New chat"
+    assert redirected_to(conn) == root_path(conn, :index)
   end
 
-  test "creates resource and redirects when data is valid", %{conn: conn} do
-    conn = post conn, chat_path(conn, :create), chat: @valid_attrs
-    assert redirected_to(conn) == chat_path(conn, :index)
-    assert Repo.get_by(Chat, @valid_attrs)
+  test "creates resource and redirects when recaptcha is verified", %{conn: conn} do
+    conn = post conn, chat_path(conn, :create, "g-recaptcha-response": true), chat: @valid_attrs
+    user_id = conn.assigns[:current_user].id
+    token =
+      redirected_to(conn)
+      |> String.split("/")
+      |> List.last
+
+    assert Repo.get_by(Chat, token: token, user_id: user_id)
   end
 
-  test "does not create resource and renders errors when data is invalid", %{conn: conn} do
+  test "does not create resource and redirects to root when recaptcha response is invalid", %{conn: conn} do
     conn = post conn, chat_path(conn, :create), chat: @invalid_attrs
-    assert html_response(conn, 200) =~ "New chat"
+    assert redirected_to(conn) == root_path(conn, :index)
   end
 
-  test "shows chosen resource", %{conn: conn} do
-    chat = Repo.insert! %Chat{}
-    conn = get conn, chat_path(conn, :show, chat)
-    assert html_response(conn, 200) =~ "Show chat"
+  # test "shows chosen resource if participant link exists for current user", %{conn: conn} do
+  #   user = fixture!(:user)
+  #   chat = fixture!(:chat)
+  #   link = Participant.changeset(%Participant{user: user, chat: chat}) |> Repo.insert!
+  #   conn = put_session(conn, :user_id, user.auth_key)
+
+  #   conn = get conn, chat_path(conn, :show, chat.token)
+  #   assert html_response(conn, 200) =~ "Show chat"
+  # end
+
+  test "redirects to new participant page if no participant link exists for current user", %{conn: conn} do
+    chat = fixture!(:chat)
+    conn = get conn, chat_path(conn, :show, chat.token)
+    assert redirected_to(conn) == chat_participant_path(conn, :new, chat.token)
   end
 
   test "renders page not found when id is nonexistent", %{conn: conn} do
     assert_error_sent 404, fn ->
       get conn, chat_path(conn, :show, -1)
     end
-  end
-
-  test "renders form for editing chosen resource", %{conn: conn} do
-    chat = Repo.insert! %Chat{}
-    conn = get conn, chat_path(conn, :edit, chat)
-    assert html_response(conn, 200) =~ "Edit chat"
-  end
-
-  test "updates chosen resource and redirects when data is valid", %{conn: conn} do
-    chat = Repo.insert! %Chat{}
-    conn = put conn, chat_path(conn, :update, chat), chat: @valid_attrs
-    assert redirected_to(conn) == chat_path(conn, :show, chat)
-    assert Repo.get_by(Chat, @valid_attrs)
-  end
-
-  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
-    chat = Repo.insert! %Chat{}
-    conn = put conn, chat_path(conn, :update, chat), chat: @invalid_attrs
-    assert html_response(conn, 200) =~ "Edit chat"
-  end
-
-  test "deletes chosen resource", %{conn: conn} do
-    chat = Repo.insert! %Chat{}
-    conn = delete conn, chat_path(conn, :delete, chat)
-    assert redirected_to(conn) == chat_path(conn, :index)
-    refute Repo.get(Chat, chat.id)
   end
 end
