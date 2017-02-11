a612f2aa2e2cd203396b0a1dfa70d1fde029b747
Micah Brown <micahyb@gmail.com>
2017-02-11 17:05:39 -0600

    added message model tests

--- test/models/message_test.exs
+++ test/models/message_test.exs
@@ -1,18 +1,35 @@
 defmodule Daychat.MessageTest do
   use Daychat.ModelCase
 
+  import Daychat.Fixtures
+
   alias Daychat.Message
 
   @valid_attrs %{body: "some content"}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
-    changeset = Message.changeset(%Message{}, @valid_attrs)
+    user = fixture!(:user)
+    chat = fixture!(:chat)
+
+    changeset = Message.changeset(%Message{user: user, chat: chat}, @valid_attrs)
     assert changeset.valid?
   end
 
   test "changeset with invalid attributes" do
     changeset = Message.changeset(%Message{}, @invalid_attrs)
     refute changeset.valid?
+  end
+
+  test "log changeset with valid attributes" do
+    chat = fixture!(:chat)
+
+    log_changeset = Message.log_changeset(%Message{chat: chat}, @valid_attrs)
+    assert log_changeset.valid?
+  end
+
+  test "log changeset with invalid attributes" do
+    log_changeset = Message.log_changeset(%Message{}, @invalid_attrs)
+    refute log_changeset.valid?
   end
 end
