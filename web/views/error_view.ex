0138ba730f4e8d665286faf6f21d956a37b004ab
Micah Brown <micahyb@gmail.com>
2017-02-14 22:43:38 -0600

    custom 404 or 500 error pages

--- web/views/error_view.ex
+++ web/views/error_view.ex
@@ -1,13 +1,13 @@
 defmodule Daychat.ErrorView do
   use Daychat.Web, :view
 
-  def render("404.html", _assigns) do
-    "Page not found"
-  end
+  # def render("404.html", _assigns) do
+  #   "Page not found"
+  # end
 
-  def render("500.html", _assigns) do
-    "Internal server error"
-  end
+  # def render("500.html", _assigns) do
+  #   "Internal server error"
+  # end
 
   # In case no render clause matches or no
   # template is found, let's render it as 500
