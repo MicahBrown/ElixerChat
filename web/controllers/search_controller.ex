54906e9408a1a649afc75cddfbff3561490d2194
Micah Brown <micahyb@gmail.com>
2017-02-18 22:37:37 -0600

    use case-insensitive search for token

--- web/controllers/search_controller.ex
+++ web/controllers/search_controller.ex
@@ -4,7 +4,11 @@ defmodule Daychat.SearchController do
   alias Daychat.Chat
 
   def show(conn, %{"q" => token}) do
-    chat = Repo.get_by(Chat, token: token)
+    pk    = String.downcase(token)
+    query = from c in Chat,
+              where: fragment("lower(?)", c.token) == ^pk,
+              limit: 1
+    chat  = Repo.all(query) |> List.first
 
     render(conn, "show.json", chat: chat)
   end
