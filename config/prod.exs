8fdd3d05301c156cdcf14a00952c982e92406145
Micah Brown <micahyb@gmail.com>
2017-02-20 20:07:03 -0600

    use non ssl protocol

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :daychat, Daychat.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "www.1daychat.com", port: 443],
+  url: [scheme: "http", host: "www.1daychat.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
