18090ff403fce96a49411d97da67138fcaac84e7
Micah Brown <micahyb@gmail.com>
2017-02-20 20:09:58 -0600

    disable ssl in prod

--- config/prod.exs
+++ config/prod.exs
@@ -23,7 +23,7 @@ config :daychat, Daychat.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true
+  ssl: false
 
 # Do not print debug messages in production
 config :logger, level: :info
