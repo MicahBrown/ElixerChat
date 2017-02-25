4d743d4a921e29b7576a19abc4570b8f4394e0b1
Micah Brown <micahyb@gmail.com>
2017-02-24 22:05:12 -0600

    adjusted authentication module to assign current user by default

--- lib/daychat/authentication.ex
+++ lib/daychat/authentication.ex
@@ -1,45 +1,34 @@
 defmodule Authentication do
   import Plug.Conn
 
-  def require_user(conn, params) do
-    conn =
-      unless current_user(conn) do
-        changeset = Daychat.User.changeset(%Daychat.User{})
-        user = Daychat.Repo.insert!(changeset)
+  def require_user(conn, _) do
+    unless current_user(conn) do
+      changeset = Daychat.User.changeset(%Daychat.User{})
+      user      = Daychat.Repo.insert!(changeset)
 
-        put_current_user(conn, user)
-      else
-        conn
-      end
-
-    authenticate(conn, params)
+      put_current_user(conn, user)
+      assign_current_user(conn, user)
+    else
+      conn
+    end
   end
 
   def current_user(conn) do
-    conn.assigns["current_user"] || get_current_user(conn)
+    conn.assigns["current_user"]
   end
 
-  def authenticate(conn, _) do
-    if user = get_current_user(conn) do
-      assign_current_user(conn, user)
-    else
-      halt(conn)
-    end
-  end
+  def set_current_user(conn, _) do
+    id   = get_session(conn, :user_id)
+    user = if id do Daychat.Repo.get_by(Daychat.User, auth_key: id) else nil end
 
-  defp get_current_user(conn) do
-    if id = get_session(conn, :user_id) do
-      Daychat.Repo.get_by(Daychat.User, auth_key: id)
-    else
-      nil
-    end
-  end
-
-  defp put_current_user(conn, user) do
-    put_session(conn, :user_id, user.auth_key)
+    assign_current_user(conn, user)
   end
 
   defp assign_current_user(conn, user) do
     assign(conn, :current_user, user)
+  end
+
+  defp put_current_user(conn, user) do
+    put_session(conn, :user_id, user.auth_key)
   end
 end
